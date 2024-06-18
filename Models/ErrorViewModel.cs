e52864438daf995d35e07cc728ae4ddfc4e15314
AyaBrados <italianobrados@gmail.com>
2024-06-18 09:01:56 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GwanjaLoveProto.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
